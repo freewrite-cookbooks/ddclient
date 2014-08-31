31e38b4c9d0ec57c342131e802663b35fb5bdd5a
Danny Guinther <dannyguinther@gmail.com>
2014-08-31 08:49:01 -0400

    Add metadata file

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name              'ddclient'
+maintainer        'Freewrite.org'
+maintainer_email  'dev@freewrite.org'
+license           'MIT'
+description       'Installs ddclient daemon, configuration, and service.'
+long_description  'Installs ddclient daemon, configuration, and service.'
+version           '0.0.1'
+
+recipe 'ddclient', 'Installs ddclient daemon, configuration, and service.'
+
+supports 'debian'
+supports 'ubuntu'
